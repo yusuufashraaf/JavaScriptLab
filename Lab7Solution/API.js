3376fc97bda1f8fe5d4d9d84108941d2a702b07f
youssef ashraf <yusuufashraaf@icloud.com>
2025-05-07 05:33:17 +0300

    changing API

--- Lab7Solution/API.js
+++ Lab7Solution/API.js
@@ -1,46 +1,39 @@
-const table = document.getElementById("userTable");
+async function returnData() {
+  let users = await fetch("https://jsonplaceholder.typicode.com/users");
+  let posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+  let comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  let user = await users.json();
+  let post = await posts.json();
+  let comment = await comments.json();
 
-Promise.all([
-  fetch("https://jsonplaceholder.typicode.com/users").then((data) =>
-    data.json()
-  ),
-  fetch("https://jsonplaceholder.typicode.com/posts").then((data) =>
-    data.json()
-  ),
-  fetch("https://jsonplaceholder.typicode.com/comments").then((data) =>
-    data.json()
-  ),
-]).then(([users, posts, comments]) => {
-  thead = document.createElement("tr");
-  thead.innerHTML = `
-    <th>Username</th>
-    <th>Email</th>
-    <th>Company</th>
-    <th>Geo (Lat,Lng)</th>
-    <th>Posts & Comments</th>
-  `;
-  table.appendChild(thead);
+  return { user, post, comment };
+}
 
-  users.forEach((user) => {
-    tr = document.createElement("tr");
-
-    userPosts = posts.filter((post) => post.userId === user.id);
+let tbody = document.getElementsByTagName("tbody")[0];
+let result = returnData().then((data) => {
+  data.user.forEach((element) => {
+    const userPost = data.post.filter((elem) => elem.userId === element.id);
+    console.log(userPost);
+    userPost.forEach((elem) => (postList = `<ul><li>${elem.title}</li>`));
 
     postsList = "<ul>";
-    userPosts.forEach((post) => {
-      const postComments = comments.filter((c) => c.postId === post.id);
+    userPost.forEach((post) => {
+      const postComments = data.comment.filter((c) => c.postId === post.id);
       postsList += `<li>${post.title} (Comments: ${postComments.length})</li>`;
     });
 
     postsList += "</ul>";
+    let tr = document.createElement("tr");
     tr.innerHTML = `
-      <td>${user.username}</td>
-      <td>${user.email}</td>
-      <td>${user.company.name}</td>
-      <td>${user.address.geo.lat}, ${user.address.geo.lng}</td>
-      <td>${postsList}</td>
-    `;
+    <td>${element.name}</td>
+    <td>${element.email}</td>
+    <td>${element.company.name}</td>
+    <td>${element.address.geo.lat}\n${element.address.geo.lng} </td>
+    <td>${postsList}</td>
 
-    table.appendChild(tr);
+    `;
+    tbody.appendChild(tr);
   });
 });
+
+
